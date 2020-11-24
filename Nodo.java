573767133fd52fd9bad7767d0afb3e2698817aec
Atomic-BRKZ <61020669+Atomic-BRKZ@users.noreply.github.com>
2020-11-23 22:34:26 -0600

    Update Nodo.java

--- Nodo.java
+++ Nodo.java
@@ -1,14 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Main;
 
-/**
- *
- * @author jairo
- */
 public class Nodo {
     int dato;
     Nodo izquierda = null;
