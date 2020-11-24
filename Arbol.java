34b9068ac0777286c4060899d98419392e905cdd
Atomic-BRKZ <61020669+Atomic-BRKZ@users.noreply.github.com>
2020-11-23 23:12:47 -0600

    Update Arbol.java

--- Arbol.java
+++ Arbol.java
@@ -1,18 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Main;
 //Insercion
 //Eliminacion
 //Recorridos. InOrden, Postorden, Preorden
 //Busqueda de elementos
 
-/**
- *
- * @author jairo
- */
 public class Arbol {
 
     Nodo raiz = null;
