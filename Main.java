91a799fcc494b4a275e629d7d11880c8b1623e07
Atomic-BRKZ <61020669+Atomic-BRKZ@users.noreply.github.com>
2020-11-23 21:17:54 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+/**
+ *
+ * @author jairo
+ */
+public class Main {
+    public static void main(String[] args) {
+        Arbol arbolito = new Arbol();
+        arbolito.insertar(50);
+        arbolito.insertar(30);
+        arbolito.insertar(20);
+        arbolito.insertar(40);
+        arbolito.insertar(70);
+        arbolito.insertar(60);
+        arbolito.insertar(80);
+        arbolito.encontrar(70);
+        
+        
+        arbolito.inorder();
+        arbolito.borrar(70);        
+        System.out.println("xd");
+        arbolito.encontrar(70);
+        arbolito.inorder();
+    }
+    
+}
